46421c8fbf7c0bbe5b3fa8e6212582c256a253a8
Abdoullah TAHRI JOUTI <tj.abfoullah@gmail.com>
2021-06-24 19:14:10 +0400

    Adding comments to the python file

--- python_scripts/move.py
+++ python_scripts/move.py
@@ -1,10 +1,16 @@
 import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BOARD)
+
+# pins we are playing with
 control_pins = [7,11,13,15]
+
+# resetting the pins
 for pin in control_pins:
   GPIO.setup(pin, GPIO.OUT)
   GPIO.output(pin, 0)
+
+# Getting the right signals to turn the stepper motor
 halfstep_seq = [
   [1,0,0,0],
   [1,1,0,0],
@@ -16,6 +22,7 @@ halfstep_seq = [
   [1,0,0,1]
 ]
 
+# Moving the stepper motor by 32th of a full circle
 for i in range(16):
  for halfstep in range(8):
    for pin in range(4):
